9e9b081761702d84ccfa3b3cc16140b24336d880
LSuper <120311070@qq.com>
2022-12-26 22:24:45 +0800

    some improve

--- ClientTest.cpp
+++ ClientTest.cpp
@@ -11,10 +11,12 @@
 int main(int argc, char* argv[])
 {
 	std::string addr;
+	std::string msg;
 	if (argc < 2)
 	{
 		std::cout << "no ip and port,will use default ip and port" << std::endl;
 		addr = {"tcp://127.0.0.1:20777"};
+		msg = "DisksOverview";
 //		return 0;
 	}
 	else
@@ -22,6 +24,7 @@ int main(int argc, char* argv[])
 		std::string ip(argv[1]);
 		std::string port(argv[2]);
 		addr = {"tcp://" + ip + ":" + port};
+		msg = argv[3];
 	}
 
 	// initialize the zmq context with a single IO thread
@@ -33,7 +36,7 @@ int main(int argc, char* argv[])
 //	socket.connect("tcp://localhost:20777");
 	socket.connect(addr);
 	// set up some static data to send
-	const std::string data{ argv[3] };
+	const std::string data{ msg };
 	for (auto request_num = 0; request_num < 10; ++request_num)
 	{
 		// initialize a request message
